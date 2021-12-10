dbdd381e3b1ca95bc481e0b4f9342b66e0108d36
sanya <sanyagoyal2001@gmail.com>
2021-12-10 11:58:30 +0530

    logo

--- components/Header.js
+++ components/Header.js
@@ -39,9 +39,7 @@ function Header() {
         <div >
             
          <Row>
-         <div>
-        <Image  src="https://raw.githubusercontent.com/sanyagoyal2000/Pictures/59a379a9cc89f2108bfaa6d5871a881b1dcc6a04/final%20(150%20x%2060%20px)%20(50%20x%2040%20px).svg" layout="fill" objectFit="contain" objectPosition="left"/>
-        </div>
+        
     <Col md="5" className="mx-auto w-full h-1/5 mt-0">
         
       <UncontrolledCarousel items={items}  />
--- components/Nav.js
+++ components/Nav.js
@@ -1,6 +1,5 @@
 import Image from "next/image";
-
-
+import logo from "./img/LOGO.svg";
 
 
 function Nav() {
@@ -8,7 +7,7 @@ function Nav() {
         
             <header > 
            <div className="relative flex items-center h-10 cursor-pointer my-auto">
-               <Image  src="https://raw.githubusercontent.com/sanyagoyal2000/Pictures/59a379a9cc89f2108bfaa6d5871a881b1dcc6a04/final%20(150%20x%2060%20px)%20(50%20x%2040%20px).svg" layout="fill" objectFit="contain" objectPosition="left"/>
+               <Image  src={logo} layout="fill" objectFit="contain" objectPosition="left"/>
            </div>
            
         </header>
